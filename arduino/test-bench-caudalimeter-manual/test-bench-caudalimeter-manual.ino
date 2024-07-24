ac84ca860ae58c37cd9c36aebc2036d3d7922ca0
Fernando Claverino <fernando.claverino@gmail.com>
2024-07-24 16:15:33 -0300

    cleanup

--- arduino/test-bench-caudalimeter-manual/test-bench-caudalimeter-manual.ino
+++ arduino/test-bench-caudalimeter-manual/test-bench-caudalimeter-manual.ino
@@ -9,16 +9,10 @@
 #define MAX_LEVEL_BUTTON_PIN 3
 #define MIN_LEVEL_BUTTON_PIN 4
 #define PUMP_RELAY_PIN 7
-#define WATER_DISTANCE_TRIGGER_PIN 8
-#define WATER_DISTANCE_ECHO_PIN 9
 
 #define TANK_MIN_WATER_LEVEL_DISTANCE 19.0
 #define TANK_MAX_WATER_LEVEL_DISTANCE 4.0
 #define TANK_VOLUME 3000
-#define TANK_WAIT_FOR_READ_DISTANCE 100
-
-#define CAUDALIMETER_BUFFER_SIZE 60
-#define CAUDALIMETER_SAMPLE_RATE 1000
 
 #define BUTTON_DEBOUNCE_TIME 50
 
@@ -27,13 +21,11 @@
 
 Relay pumpRelay(PUMP_RELAY_PIN);
 Tank tank(TANK_MIN_WATER_LEVEL_DISTANCE, TANK_MAX_WATER_LEVEL_DISTANCE, TANK_VOLUME);
-Wait waitforReadDistance(TANK_WAIT_FOR_READ_DISTANCE);
 Button maxLevelButton(MAX_LEVEL_BUTTON_PIN, BUTTON_DEBOUNCE_TIME);
 Button minLevelButton(MIN_LEVEL_BUTTON_PIN, BUTTON_DEBOUNCE_TIME);
 
 Caudalimeter caudalimeter(CAUDALIMETER_PIN);
 
-// Wait waitforPublish(WAIT_FOR_PUBLISH_LOOP, WAIT_FOR_PUBLISH_START);
 Wait waitforDebug(WAIT_FOR_PUBLISH_LOOP);
 
 void caudalimeterTick() {
@@ -52,10 +44,12 @@ void loop() {
 
   if (maxLevelButton.isToggle()) {
     tank.setWaterLevelOnMax();
+    caudalimeter.reset();
   }
 
   if (minLevelButton.isToggle()) {
     tank.setWaterLevelOnMin();
+    publishMetrics(tank, caudalimeter);
   }
 
   if (tank.isMinLevel()) {
@@ -66,14 +60,25 @@ void loop() {
     pumpRelay.off();
   }
 
+  // debug(tank, caudalimeter);
+}
+
+void publishMetrics(Tank tank, Caudalimeter caudalimeter) {
+  Serial.print(tank.getEmptiedTimeSpanInSeconds());
+  Serial.print(", ");
+
+  Serial.print(caudalimeter.getTickCount());
+  Serial.println();
+}
+
+void debug(Tank tank, Caudalimeter caudalimeter) {
   if (waitforDebug.done()) {
-    noInterrupts();
-
+    // There is a serial and interrupts issue: https://forum.arduino.cc/t/nointerrupts-and-serial-write-issues/140133
+    // but I don´t have it
+    // noInterrupts();
     publishDebug(tank, caudalimeter);
-
-    interrupts();
+    // interrupts();
   }
-
 }
 
 void publishDebug(Tank tank, Caudalimeter caudalimeter) {
