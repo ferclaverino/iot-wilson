b7d2992e943fa65c626887387f0d83a63a81ec34
Fernando Claverino <fernando.claverino@gmail.com>
2024-07-26 11:47:47 -0300

    extract domain setup

--- arduino/test-bench-caudalimeter-mock/test-bench-caudalimeter-mock.ino
+++ arduino/test-bench-caudalimeter-mock/test-bench-caudalimeter-mock.ino
@@ -2,6 +2,10 @@
 #include "src/time/Wait.h"
 #include "src/domain/TicksFrequency.h"
 
+// Domiain setup
+#define MIN_FREQUENCY 5
+#define MAX_FREQUENCY 10
+
 // Waiters setup
 #define WAIT_FOR_PUBLISH_LOOP 5000
 #define WAIT_FOR_PUBLISH_START 5000
@@ -21,17 +25,16 @@ void setup() {
 }
 
 long totalTicksCounts = 0;
+int minPulseCount = MIN_FREQUENCY * (WAIT_FOR_PUBLISH_LOOP / 1000);
+int maxPulseCount = MAX_FREQUENCY * (WAIT_FOR_PUBLISH_LOOP / 1000);
 
 void loop() {
 
   if (waitforPublish.done()) {
-
-    // Frequency will be between 1 (5/5) and 5 (25/5) hz
-    long pulseCount = random(5, 25);
+    long pulseCount = random(minPulseCount, maxPulseCount);
     // metricsGateway.publishPulseCount(pulseCount);
 
     totalTicksCounts += pulseCount;
-
     unsigned int timeSpan = waitforPublish.getDoneTimeSpanInMillis();
     float frequency = ticksFrequency.getFrequencyInHz(totalTicksCounts, timeSpan);
 
