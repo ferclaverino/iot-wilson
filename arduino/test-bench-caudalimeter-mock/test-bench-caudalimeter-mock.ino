1f0307b94b94fea475c8ef9d7843efe46cc2102a
Fernando Claverino <fernando.claverino@gmail.com>
2024-07-26 11:34:16 -0300

    fix total ticks count

--- arduino/test-bench-caudalimeter-mock/test-bench-caudalimeter-mock.ino
+++ arduino/test-bench-caudalimeter-mock/test-bench-caudalimeter-mock.ino
@@ -3,7 +3,7 @@
 #include "src/domain/TicksFrequency.h"
 
 // Waiters setup
-#define WAIT_FOR_PUBLISH_LOOP 1000
+#define WAIT_FOR_PUBLISH_LOOP 5000
 #define WAIT_FOR_PUBLISH_START 5000
 
 // Domain
@@ -20,15 +20,20 @@ void setup() {
   randomSeed(analogRead(0));
 }
 
+long totalTicksCounts = 0;
+
 void loop() {
 
   if (waitforPublish.done()) {
 
+    // Frequency will be between 1 (5/5) and 5 (25/5) hz
     long pulseCount = random(5, 25);
     // metricsGateway.publishPulseCount(pulseCount);
 
+    totalTicksCounts += pulseCount;
+
     unsigned int timeSpan = waitforPublish.getDoneTimeSpanInMillis();
-    float frequency = ticksFrequency.getFrequencyInHz(pulseCount, timeSpan);
+    float frequency = ticksFrequency.getFrequencyInHz(totalTicksCounts, timeSpan);
 
     metricsGateway.publishFrequency(frequency, timeSpan);
   }
